adcf0589987a8fe43c754f38a7a076f49b99211b
Andrew Pierce <andrunix@gmail.com>
2017-03-20 18:24:32 -0400

    Here is my take on it.
    I added the package.json so you get the mysql module loaded.
    Reworked materialFinder to only use one sql statement.
    Organized the code a bit.
    
    Just my thoughts. You can ignore this if you'd like.

--- materialFinder.js
+++ materialFinder.js
@@ -1,103 +1,69 @@
 var mysql = require("mysql");
 var parsedData;
-var listData=[];
+var listData = [];
 var material;
+var connection;
 
-function mFinder(callback) {
-	// First you need to create a connection to the db
-	var connection = mysql.createConnection({
-  	host: "localhost",
-  	user: "root",
-	password: "Ishtar842"
+function closeConnection() {
+  connection.end(function(err) {
+  });
+
+  process.exit(0);
+}
+
+var lookup = function() {
+	mFinder(function(listData) {
+		console.log('listData size: ' + listData.length);
+
+     listData.forEach(function(row) {
+      console.log('row: ' + JSON.stringify(row));
+    });
+
+    closeConnection();
 	});
-
-	connection.connect(function(err){       
-		{
-  		if(err){
-    		console.log('Error connecting to Db');
-    		return;
-		} 
-		}	
-  	console.log('Connection established');
-	});
-
-	var itemName = process.argv[2];
-	
-	connection.query("SELECT MAT.typeID, TYP.typeName, MAT.materialTypeID, " +
-                "MAT.quantity, TYP.groupID, TYP.graphicID " +
-                "FROM eveql.invTypeMaterials as MAT " +
-                "INNER JOIN eveql.invTypes as TYP " +
-                "ON MAT.typeID = TYP.typeID and TYP.typeName " +
-                "= CONCAT('" + itemName +"');", 
-	function(err, rows){
-                  if(err){
-                    console.log(err);
-                  }else{
-                    for(row in rows){
-                  listData.push(JSON.parse(JSON.stringify(rows[row])));
-                  //console.log('Value: ' + JSON.stringify(listData[row]));
-                  }
-                //console.log("About to return : " + JSON.stringify(listData));
-		callback(listData);
-		}
-   
-	});
-
-	connection.end(function(err) {
-  	// The connection is terminated gracefully
- 	// Ensures all previously enqueued queries are still
-  	// before sending a COM_QUIT packet to the MySQL server.
-	});
-	
-
 };
 
-mFinder(function(listData){	
-	for(row in listData){
-		var connection = mysql.createConnection({
-        		host: "localhost",
-        		user: "root",
-        		password: "Ishtar842"
-        	});
 
-        	connection.connect(function(err){
-                	{
-                		if(err){
-                		console.log('Error connecting to Db');
-                		return;
-                		}
-                	}
-        		
-        	});
+function openConnection(next) {
+	var conn = mysql.createConnection({
+  	host: "localhost",
+  	user: "root",
+    password: "",
+		database: 'eveql'
+	});
 
-        	connection.query("SELECT typeID and typeName FROM eveql.invTypes as TYP WHERE" +
-                	" TYP.typeID = " + listData[row].materialTypeID  + ";",
-        	function(err, rows){
-                	if(err){
-                    		console.log(err);
-                  	}else{
-                    		for(row in rows){
-				temp.push(JSON.parse(JSON.stringify(rows[row])));
-                  			for(data in listData){
-				console.log('A:: ' + JSON.stringify(listData[data]));
-				console.log('B:: ' +JSON.stringify(listData[data].materialTypeID));
-				console.log('C:: ' + JSON.stringify(rows[row].typeID));
-						if(listData[data].materialTypeID === temp[row].typeID){
-						listData[data].materialTypeName = temp[row].typeName;
-						}
-					}
-				listData.materialTypeName = (JSON.stringify(rows[row]));
-                		console.log('Value: ' + JSON.stringify(listData));
-                  		}
-                	//console.log("About to return : " + JSON.stringify(listData));
-                	}
+	conn.connect(function(err) {       
+  		if(err) {
+    		console.log('Error connecting to Db');
+    		return;
+		  }	
+    console.log('Connection established');
+		connection = conn;
+		next();
+	});
+}
 
-        	});
 
-        	connection.end(function(err) {
-        	// The connection is terminated gracefully
-        	// Ensures all previously enqueued queries are still
-        	// before sending a COM_QUIT packet to the MySQL server.
-        	});
-	}	
-});
+function mFinder(callback) {
+	var itemName = process.argv[2];
+  var sql = "SELECT t.typeID, m.typeID, t.typeName, m.materialTypeID, m.quantity, t.groupID, t.graphicID " +
+    "FROM invTypes t INNER JOIN invTypeMaterials m ON t.typeID = m.typeID " +
+    "WHERE t.typeName = '" + itemName + "'";
+								 
+	connection.query(sql, function(err, rows) {
+		console.log('in the callback of the query');
+		console.log('rows.length: ' + rows.length);
+		
+		if(err) console.log(err);
+		else {
+			callback(rows);
+		}
+	});
+};
+
+openConnection(lookup);
+
+
+
+
+
