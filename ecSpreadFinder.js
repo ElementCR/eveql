94380f30849474baed4394f6521de1adecefc90f
Carlos Rodriguez <Element.cr@gmail.com>
2017-03-21 23:18:01 -0400

    Basic economic data storage
    This change creates a foundation for the data-objects that we will be using in order to transfer economic data from the service we are using towards the MySQL Database. 
    
    The idea is to iterate through every item in the future economic_data table within MySQL and automate the call to the service where we then store the data values. This will run once daily in the future to reduce the demand that will be put on eve-central servers.
    
    Note - Still need to remove non-tradeable goods from the table. 
    Still have not designed a way to save historical data for trend analysis.
    
    Data that we are worried about for the transfer are - 
    1) Highest Buy Order Values
    2) Lowest Sell Order Values
    3) Profit Spread
    4) Percentage Spread
    5) Buy Volume (24 Hours)
    6) Sell Volume (24 Hours)
    7) All Volume (Buy + Sell (24 Hours))

--- ecSpreadFinder.js
+++ ecSpreadFinder.js
@@ -1,20 +1,54 @@
 var querystring = require('querystring');
 var http = require('http');
+var buy;
+var all;
+var sell;
+
+var itemStats = {
+  "hiBuy": 0,
+  "loSel": 0,
+  "buyVol": 0,
+  "selVol": 0,
+  "allVol": 0,
+  "iskSpread": 0,
+  "perSpread": 0
+}
 
 var host = 'http://api.eve-central.com/api/marketstat'
 
 var options = {
-	host: 'api.eve-central.com',
-	port: 80,
-	path: '/api/marketstat/json?typeid=34&usessystem=30000142',
-	method: 'GET'
+  host: 'api.eve-central.com',
+  port: 80,
+  path: '/api/marketstat/json?typeid=34&usessystem=30000142',
+  method: 'GET'
 }
 
-http.request(options, function(res) {
+http.request(options, function (res) {
   console.log('STATUS: ' + res.statusCode);
   console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
     console.log('BODY: ' + chunk);
+    var jsonBuy = JSON.parse(chunk);
+
+    buy = jsonBuy[0]["buy"];
+    all = jsonBuy[0]["all"];
+    sell = jsonBuy[0]["sell"];
+
+    //console.log("buy Xfer :: " + JSON.stringify(buy));
+    //console.log("all Xfer :: " + JSON.stringify(all));
+    //console.log("sell Xfer :: " + JSON.stringify(sell));
+    //console.log("raw item stats :: " + JSON.stringify(itemStats));
+
+    itemStats.hiBuy = buy["max"];
+    itemStats.loSel = sell["min"];
+    itemStats.iskSpread = itemStats.hiBuy - itemStats.loSel;
+    itemStats.perSpread = itemStats.iskSpread / itemStats.hiBuy;
+    itemStats.buyVol = buy["volume"];
+    itemStats.selVol = sell["volume"];
+    itemStats.allVol = all["volume"];
+
+    console.log("Transferred Values" + JSON.stringify(itemStats));
+
   });
 }).end();
